fe534c5218675e8745415d5249ae264409168039
Jessica David <jessicamdavid@gmail.com>
2013-02-21 20:44:13 -0500

    Fixed some connection issues, can succesfully get and parse JSON and open a socket to a port

--- src/com/example/csc2231_project/MainActivity.java
+++ src/com/example/csc2231_project/MainActivity.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
@@ -15,13 +17,14 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder.AudioSource;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,11 +38,10 @@ public class MainActivity extends Activity {
 	private static final int CHANNEL = AudioFormat.CHANNEL_IN_STEREO;
 	private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 	
-	private static final String SERVER = "127.0.0.1";
-	private static final int CONNECT_PORT = 1234;
-	private static final int AUDIO_PORT = 2345;
-	private static Socket connectSocket = null;
+	private static final String SERVER = "192.168.74.140";
+	private static final String AUDIO_PORT = "4444";
 	private static Socket audioSocket = null;
+	private static PrintWriter audioWrite;
 	
 	private static String id;
 	private static byte[] idByte;
@@ -60,17 +62,11 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);       
         
-        String connectInfo = pingServer();
-        try {
-			JSONArray infoArray = new JSONArray(connectInfo);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        serverConnect();
-                
+        String[] params = {SERVER, AUDIO_PORT};
+        new ServerConnectTask().execute(params);
+        
         id = "A2D4A2D4";
         idByte = id.getBytes();
         time = System.currentTimeMillis();
@@ -80,23 +76,47 @@ public class MainActivity extends Activity {
     	
         txt = (TextView) findViewById(R.id.record_text);
         recordButton = (ToggleButton) findViewById(R.id.record_button);
+        recordButton.setEnabled(false);
     }
-
-    /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    } */
+    
+	private class ServerConnectTask extends AsyncTask<String, Void, Void> {
+		@Override
+		protected Void doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			try {
+			    JSONObject connectInfo = new JSONObject(pingServer());
+				audioSocket = new Socket(params[0], connectInfo.getInt("port"));
+			} catch (NumberFormatException e) {
+				Log.e(TAG, "error in NumberFormatException");
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+			 	Log.e(TAG, "error in UnknownHost");
+			 	e.printStackTrace();
+			} catch (IOException e) {
+			 	Log.e(TAG, "error in IOException");
+			 	e.printStackTrace();
+			} catch (JSONException e) {
+				Log.e(TAG, "JSON IS AWFULLL");
+				e.printStackTrace();
+			}
+			
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			recordButton.setEnabled(true);
+		}
+	}
     
     private String pingServer() {
 
     	StringBuilder builder = new StringBuilder();
     	HttpClient client = new DefaultHttpClient();
-    	HttpGet get = new HttpGet(SERVER + "/connect");
+    	HttpGet get = new HttpGet("http://www.cs.toronto.edu/~jdavid/csc2231_site/connect");
 		
     	try {
-    		HttpResponse response;
-			response = client.execute(get);
+    		HttpResponse response = client.execute(get);
     		StatusLine status = response.getStatusLine();
     		int code = status.getStatusCode();
     		if (code == 200) {
@@ -107,38 +127,19 @@ public class MainActivity extends Activity {
     			while((line = reader.readLine()) != null) {
     				builder.append(line);
     			}
+    			reader.close();
+    			content.close();
     		} else {
     			Log.e(TAG, "Error: could not get connect JSON");
     		}
 
 		} catch (ClientProtocolException e) {
 			Log.e(TAG, "ClientProtocolException in serverConnect");
-			// e.printStackTrace();
 		} catch (IOException e) {
 			Log.e(TAG, "IOException in serverConnect");
-			// e.printStackTrace();
 		}
     	
     	return builder.toString();
-    }
-    
-    private void serverConnect() {
-    	
-    	/* BufferedReader connectResponse;
-    	PrintWriter connectWrite;
-    	try {
-    		connectSocket = new Socket(SERVER, CONNECT_PORT);
-    		connectWrite = new PrintWriter(connectSocket.getOutputStream(), true);
-    		connectResponse = new BufferedReader(new InputStreamReader(connectSocket.getInputStream()));
-    		
-    		connectWrite.println("");
-    	} catch (IOException e) {
-    		
-    	} finally {
-    		// connectResponse.close();
-    		// connectWrite.close();
-    		// connectSocket.close();
-    	} */
     }
     
     private void prepare_recording() {
@@ -165,6 +166,14 @@ public class MainActivity extends Activity {
     		txt.setText(R.string.record_stop);
     	} else {
     		Log.v(TAG, "Trying to stop recording...");
+    		audioWrite.println("STOP");
+    		audioWrite.close();
+    		try {
+				audioSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     		
     		currentlyRecording = false;
     		audioRecorder.stop();
@@ -180,6 +189,12 @@ public class MainActivity extends Activity {
     public void write_audio() {
     	byte[] audio_data = new byte[audioBufferSize];
     	int read = 0;
+    	try {
+			audioWrite = new PrintWriter(audioSocket.getOutputStream(), true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	
     	if(audioRecorder != null) {
     		while(currentlyRecording) {
@@ -191,6 +206,7 @@ public class MainActivity extends Activity {
     				System.arraycopy(timeByte, 0, packetData, idByte.length, timeByte.length);
     				System.arraycopy(audio_data, 0, packetData, idByte.length + timeByte.length, audio_data.length);
     				
+    				// TODO: change this code so it actually does something useful
     				String output = "";
     				for(int b = 0; b < 30; b++) {
     					output += packetData[b];
@@ -200,9 +216,9 @@ public class MainActivity extends Activity {
     						output += ", ";
     					}
     				}
-    				output += "etc.";
+    				output += "etc.\n";
     				
-    				Log.v(TAG, output);
+    				audioWrite.println(output);
     			}
     		}
     	}
@@ -216,44 +232,3 @@ public class MainActivity extends Activity {
 	    return buf.array();
 	}
 }
-
-/*audio_recorder = new MediaRecorder();
-audio_recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-audio_recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-audio_recorder.setOutputFile(file_name);
-audio_recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);*/        	
-/*try {
-audio_recorder.prepare();
-} catch (IOException e) {
-Log.e(TAG, "failed on preparing mic");
-}*/
-
-// audio_recorder.start();
-
-
-// String file = set_file_name();
-// String file = Environment.getExternalStorageDirectory().getAbsolutePath() + "/csc2231/temp.raw";
-// Log.v(TAG, file);
-// FileOutputStream recording = null;
-
-// try {
-//	recording = new FileOutputStream(file);
-// } catch (FileNotFoundException e) {
-// 	Log.e(TAG, "File not found exception in write_audio");
-// 	e.printStackTrace();
-// }
-
-
-
-// try {
-	// recording.write(audio_data);
-	// Log.v(TAG, "Writing data");
-// } catch (IOException e) {
-// 	Log.e(TAG, "Error writing data in write_audio");
-// }
-// try {
-// 	recording.close();
-// } catch (IOException e) {
-// 	Log.e(TAG, "Error closing file in write_audio");
-// }
-// }
