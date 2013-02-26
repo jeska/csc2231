958e29a3751cfd7d3dfb2c53cac114aac429f34f
Jessica David <jessicamdavid@gmail.com>
2013-02-25 19:02:33 -0500

    Some code tidying

--- src/com/example/csc2231_project/MainActivity.java
+++ src/com/example/csc2231_project/MainActivity.java
@@ -10,9 +10,7 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Random;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
@@ -38,41 +36,38 @@ public class MainActivity extends Activity {
 	/* Log tag */
 	private static final String TAG = "CSC2231";
 	
-	/* Microphone information 
+	/* Microphone information and recording variables
 	 * TODO: have this get information from server 
 	 * */
 	private static final int MIC = AudioSource.MIC;
 	private static final int SAMPLE_RATE = 44100;
 	private static final int CHANNEL = AudioFormat.CHANNEL_IN_MONO;
 	private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+	private AudioRecord audioRecorder = null;
+	private Thread recordThread = null;
+	private boolean currentlyRecording = false;
+	private int audioBufferSize;
 	
 	/* connection and socket information 
 	 * currently the IP is the external for my work machine
 	 * */
 	private static final String SERVER = "192.168.74.140";
+	private static final String CONNECT_PORT = "2444";
 	private static int audioPort;
 	private static DatagramSocket audioSocket;
-	// private static Socket audioSocket = null;
-	// private static PrintWriter audioWrite;
-	// private static DataOutputStream audioDataOutput;
 	
+	/* Byte arrays that get sent in the packet */
 	private static long id;
 	private static byte[] idByte;
-	// private static long time;
 	private static byte[] timeByte;
 	private static long count;
-	private static int size;
+	private static byte[] sizeByte;
+	private byte[] packetData;
 	
+	/* Android view objects */
 	private ToggleButton recordButton;
 	private TextView txt;
 	
-	
-	private AudioRecord audioRecorder = null;
-	private Thread recordThread = null;
-	private boolean currentlyRecording = false;
-	private int audioBufferSize;
-	private byte[] packetData;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);        
@@ -81,6 +76,7 @@ public class MainActivity extends Activity {
         new ServerConnectTask().execute();
         
     	audioBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, CHANNEL, ENCODING);
+		sizeByte = getBytes(audioBufferSize);
     	
         txt = (TextView) findViewById(R.id.record_text);
         recordButton = (ToggleButton) findViewById(R.id.record_button);
@@ -107,12 +103,13 @@ public class MainActivity extends Activity {
 		
 		@Override
 		protected void onPostExecute(Void result) {
+			idByte = getBytes(id);
 	    	packetData = new byte[8 + // idByte.length
 	    	                      8 + // timeByte.length
 	    	                      8 + // num samples in
-	    	                      4 + // size?
+	    	                      4 + // size of audio buffer
 	    	                      audioBufferSize];
-	    	count = 231;
+	    	count = 231; // TODO: Not this!
 			recordButton.setEnabled(true);
 		}
 	}
@@ -121,15 +118,13 @@ public class MainActivity extends Activity {
 
     	StringBuilder builder = new StringBuilder();
     	HttpClient client = new DefaultHttpClient();
-    	HttpGet get = new HttpGet("http://192.168.74.140:2444/connect");
+    	HttpGet get = new HttpGet(SERVER + ":" + CONNECT_PORT + "/connect");
 		
     	try {
     		HttpResponse response = client.execute(get);
     		StatusLine status = response.getStatusLine();
-    		int code = status.getStatusCode();
-    		if (code == 200) {
-    			HttpEntity entity = response.getEntity();
-    			InputStream content = entity.getContent();
+    		if (status.getStatusCode() == 200) {
+    			InputStream content = response.getEntity().getContent();
     			BufferedReader reader = new BufferedReader(new InputStreamReader(content));
     			String line;
     			while((line = reader.readLine()) != null) {
@@ -142,20 +137,26 @@ public class MainActivity extends Activity {
     		}
 
 		} catch (ClientProtocolException e) {
+			// TODO: Something better here?
 			Log.e(TAG, "ClientProtocolException in serverConnect");
 		} catch (IOException e) {
+			// TODO: Something better here?
 			Log.e(TAG, "IOException in serverConnect");
+		} finally {
+			if(builder.length() == 0) {
+				builder.append("ERROR");
+			}
 		}
     	
     	return builder.toString();
     }
     
     private void prepare_recording() {
-    	Log.v(TAG, "Buffer size: " + audioBufferSize);
     	audioRecorder = new AudioRecord(MIC, SAMPLE_RATE, CHANNEL, ENCODING, audioBufferSize);
     }
 
-    public void record_button_click(View view) {
+    // TODO: would be nice to pull some of this out into functions, like prepare_recording
+    public void recordButtonClick(View view) {
     	if(recordButton.isChecked()) {
     		currentlyRecording = true;
     		prepare_recording();
@@ -180,15 +181,6 @@ public class MainActivity extends Activity {
     		txt.setText(R.string.record_stop);
     	} else {
     		currentlyRecording = false;
-    		// audioWrite.println("STOP");
-    		// audioWrite.close();
-    		
-    		// try {
-        	 	// audioDataOutput.close();
-			// } catch (IOException e) {
-			// 	e.printStackTrace();
-			// }
-
 			audioSocket.close();
     		audioRecorder.stop();
         	audioRecorder.release();
@@ -197,20 +189,18 @@ public class MainActivity extends Activity {
         	
         	txt.setText(R.string.record_start);
     	}
-    }  
+    }
         
     public void write_audio() {
     	byte[] audio_data = new byte[audioBufferSize];
     	int read = 0;
     	try {
-			// audioWrite = new PrintWriter(audioSocket.getOutputStream(), true);
-    		// audioDataOutput = new DataOutputStream(audioSocket.getOutputStream());
 	    	if(audioRecorder != null) {
 	    		while(currentlyRecording) {
-	    			// read = audioRecorder.read(audio_data, 0, audioBufferSize);
-	    			new Random().nextBytes(audio_data);
+	    			read = audioRecorder.read(audio_data, 0, audioBufferSize);
 	    			
 	    			if(read != AudioRecord.ERROR_INVALID_OPERATION) {
+	    				// TODO: This write and the sizeByte only need to happen once. Figure out a place to move this.
 	    				System.arraycopy(idByte, 0, packetData, 0, idByte.length);
 	    		    	
 	    				timeByte = getBytes(System.currentTimeMillis());
@@ -219,23 +209,12 @@ public class MainActivity extends Activity {
 	    				byte[] countByte = getBytes(count);
 	    				System.arraycopy(countByte, 0, packetData, idByte.length + timeByte.length, countByte.length);
 	    				
-	    				byte[] sizeByte = getBytes(audioBufferSize);
 	    				System.arraycopy(sizeByte, 0, packetData, idByte.length + timeByte.length + countByte.length, sizeByte.length);
 	    				
 	    				System.arraycopy(audio_data, 0, packetData, idByte.length + timeByte.length + countByte.length + sizeByte.length, audio_data.length);
 	    				
-	    				// TODO: change this code so it actually does something useful
-	    				String output = "";
-	    				for(int b = 0; b < 50; b++) {
-	    					output += packetData[b];
-	    					output += ", ";
-	    				}
-	    				output += "etc.\n";
-	    				
-	    				// audioWrite.(packetData);
-	    				// audioDataOutput.write(packetData);
+	    				// TODO: make sure this does all it needs to do!
 	    				DatagramPacket packet = new DatagramPacket(packetData, packetData.length, InetAddress.getByName(SERVER), audioPort);
-	    				Log.v(TAG, output);
 	    				audioSocket.send(packet);
 	    			}
 	    		}
@@ -247,17 +226,13 @@ public class MainActivity extends Activity {
     
     public byte[] getBytes(int val)
    	{
-   	    ByteBuffer buf = ByteBuffer.allocate(4);
-   	    buf.order(ByteOrder.BIG_ENDIAN);
-   	    buf.putInt(val);
-   	    return buf.array();
+   	    ByteBuffer int_buffer = ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN);
+   	    return int_buffer.putInt(val).array();
    	}
     
-    public byte[] getBytes(Long val)
+    public byte[] getBytes(long val)
 	{
-	    ByteBuffer buf = ByteBuffer.allocate(8);
-	    buf.order(ByteOrder.BIG_ENDIAN);
-	    buf.putLong(val);
-	    return buf.array();
+    	ByteBuffer long_buffer = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN);
+   	    return long_buffer.putLong(val).array();
 	}
 }
